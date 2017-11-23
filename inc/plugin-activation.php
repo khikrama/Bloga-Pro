8cbc8ef6a1d24d21cd57365f39faabb561e4edfa
Khandaker Ikrama <ikrama.213@gmail.com>
2017-11-23 22:02:42 +0600

    Working on bloga pro core plugin and TGM activation

--- inc/plugin-activation.php
+++ inc/plugin-activation.php
@@ -57,6 +57,33 @@ function bloga_register_required_plugins() {
 	 * If the source is NOT from the .org repo, then source is also required.
 	 */
 	$plugins = array(
+		// Bloga Pro Core
+		array(
+			'name' => esc_html__('Bloga Pro Core', 'bloga'), // The plugin name.
+			'slug' => 'bloga-core', // The plugin slug (typically the folder name).
+			'source' => get_template_directory() . '/plugins/bloga-core.zip', // The plugin source.
+			'required' => true, // If false, the plugin is only 'recommended' instead of required.
+			'version' => '1.0.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+		),
+		// Elementor Page Builder 
+		array(
+			'name' => esc_html__('Elementor Page Builder', 'bloga'),
+			'slug' => 'elementor',
+			'required' => true,
+		),
+		// CMB2 Metabox 
+        array(
+            'name'     => esc_html__('CMB2', 'bloga'),
+            'slug'     => 'cmb2',
+            'required' => true,
+        ),
+        // Redux Framework
+        array(
+            'name'     => esc_html__('Redux FrameWork', 'bloga'),
+            'slug'     => 'redux-framework',
+            'required' => true,
+        ),
+		// Contact Form 7
         array(
             'name'     => esc_html__('Contact Form 7', 'bloga'),
             'slug'     => 'contact-form-7',
