1e5697c8d99cc2891fe2b2c70dfc6023b0d667a6
Khandaker Ikrama <ikrama.213@gmail.com>
2018-11-21 12:42:01 +0600

    Coming soon page template added

---
+++ template-pages/coming-soon.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Coming Soon
+ */
+?>
+
+<div style="height: 100%;display: flex;align-items: center;flex-direction: column;justify-content: center;">
+    
+    <h1>Coming Soon...</h1>
+    <small>&copy;<?php echo date('Y');?> <a target="_blank" href="https://xltheme.com/">XLTHEME</a>. </small>
+
+</div>
